946e7ee9acd72758057c39ca995c145728b7b8c2
sunidhi04 <sunidhi04sharma@gmail.com>
2025-03-12 21:34:39 -0700

    MainSettingsPanel
    Created everything for the SettingsCanvas up to PlayerSpeed

--- Assets/_Scripts/CoinCounterUI.cs
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -9,6 +9,8 @@ public class CoinCounterUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI toUpdate;
     [SerializeField] private Transform coinTextContainer;
     [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
+
 
     private float containerInitPosition;
     private float moveAmount;
@@ -24,6 +26,17 @@ public class CoinCounterUI : MonoBehaviour
     public void UpdateScore(int score)
     {
         toUpdate.SetText($"{score}");
-        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration);
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetCoinContainer(score));
     }
+
+    private IEnumerator ResetCoinContainer(int score)
+    {
+        yield return new WaitForSeconds(duration);
+        current.SetText($"{score}");
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x,
+        containerInitPosition, localPosition.z);
+    }
+
 }
